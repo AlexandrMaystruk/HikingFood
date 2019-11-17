3d7403b5db7b61ec1b91ee116d16667c0d363341
Alex <maystruks08@gmail.com>
2019-11-17 23:51:20 +0200

    [OPT] Refactor create menu fragment screen

--- app/src/main/java/com/gmail/maystruks08/hikingfood/ui/main/menu/createmenu/CreateMenuFragment.kt
+++ app/src/main/java/com/gmail/maystruks08/hikingfood/ui/main/menu/createmenu/CreateMenuFragment.kt
@@ -7,6 +7,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.core.content.ContextCompat
 import com.gmail.maystruks08.hikingfood.*
 import com.gmail.maystruks08.hikingfood.core.base.BaseFragment
@@ -16,6 +17,13 @@ import com.gmail.maystruks08.hikingfood.utils.extensions.toast
 import kotlinx.android.synthetic.main.fragment_create_menu.*
 import java.util.*
 import javax.inject.Inject
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.fragment_create_menu.btnCreateMenuNextStep
+import kotlinx.android.synthetic.main.fragment_create_menu.inputLayout
+import kotlinx.android.synthetic.main.fragment_create_menu.npPeopleCountValue
+import kotlinx.android.synthetic.main.fragment_create_menu.npReceptionCountValue
+import kotlinx.android.synthetic.main.fragment_create_menu.npRelaxDayCountValue
+import kotlinx.android.synthetic.main.fragment_create_menu.tvDateOfStartMenu
 
 class CreateMenuFragment : BaseFragment(), CreateMenuContract.View {
 
@@ -35,8 +43,8 @@ class CreateMenuFragment : BaseFragment(), CreateMenuContract.View {
     override fun builder(): FragmentToolbar {
         val onCLick = View.OnClickListener { presenter.onBackClicked() }
         return FragmentToolbar.Builder()
-            .withId(R.id.toolbar)
-            .withTitle( R.string.fragment_create_menu_name)
+            .withId(R.id.toolbarCreate)
+            .withTitle(R.string.fragment_create_menu_name)
             .withNavigationIcon(R.drawable.ic_arrow_back, onCLick)
             .build()
     }
@@ -50,11 +58,13 @@ class CreateMenuFragment : BaseFragment(), CreateMenuContract.View {
             }
         }
 
+        inputLayout.requestFocus()
         etMenuName.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable) {
-            enableNextStepButton(s.isNotEmpty())
+                enableNextStepButton(s.isNotEmpty())
                 presenter.onNameMenuChanged(s.toString())
             }
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
@@ -77,14 +87,15 @@ class CreateMenuFragment : BaseFragment(), CreateMenuContract.View {
             }
         }
 
-        rgStartReception.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                R.id.rbBreakfast -> presenter.onTimeMenuStartChanged(0)
-                R.id.rbLunch -> presenter.onTimeMenuStartChanged(1)
-                R.id.rbDinner -> presenter.onTimeMenuStartChanged(2)
-            }
+        rgStartReception.adapter = ArrayAdapter(context!!, R.layout.spinner_item, context!!.resources.getTextArray(R.array.menu_start)).apply {
+            setDropDownViewResource(R.layout.spinner_drop_down)
         }
-
+        rgStartReception.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                presenter.onTimeMenuStartChanged(position)
+            }
+        })
         tvDateOfStartMenu.setOnClickListener {
             presenter.onSetDateStartMenuClicked()
         }
@@ -110,9 +121,9 @@ class CreateMenuFragment : BaseFragment(), CreateMenuContract.View {
         super.onDestroy()
     }
 
-    private fun enableNextStepButton(enable: Boolean){
+    private fun enableNextStepButton(enable: Boolean) {
         context?.let {
-            btnCreateMenuNextStep.background =  if (enable) {
+            btnCreateMenuNextStep.background = if (enable) {
                 ContextCompat.getDrawable(it, R.drawable.bg_main_btn)
             } else {
                 ContextCompat.getDrawable(it, R.drawable.bg_main_inactive_btn)
